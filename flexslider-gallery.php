f5ea36205e6abf01010ace87e48a976985322cbb
Eternal-tears <leonhart.love1@gmail.com>
2013-02-18 15:41:38 +0900

    管理画面上から画像サイズの設定が可能

--- flexslider-gallery.php
+++ flexslider-gallery.php
@@ -14,9 +14,9 @@ load_plugin_textdomain( 'flexslidergallery', false, dirname( plugin_basename( __
 /* ==================================================
 ■ギャラリー画像専用の画像サイズ設定
  ================================================== */
-add_image_size( 'single-post-thumbnail', 348, 348); //大画像設定
-add_image_size( 'top-post-thumbnail', 148, 148, true); // 中画像サイズ設定
-add_image_size( 'thumbnail', 58, 58, true);//ミニサムネイル画像設定
+add_image_size( 'single-post-thumbnail', get_option('flexslidergallery_MainImgSize'), get_option('flexslidergallery_MainImgSize')); //大画像設定
+add_image_size( 'top-post-thumbnail', get_option('flexslidergallery_MediumImgSize'), get_option('flexslidergallery_MediumImgSize'), true); // 中画像サイズ設定
+add_image_size( 'mini-thumbnail', get_option('flexslidergallery_SmallImgSize'), get_option('flexslidergallery_SmallImgSize'), true);//ミニサムネイル画像設定
 
 /* ==================================================
 ■サムネイル画像とメイン画像の表示ソース
@@ -149,13 +149,16 @@ function flexslidergallery_init_options() {
 	// 初期化の処理を行う
 	if (!get_option('flexslidergallery_installed')) {
 		update_option('flexslidergallery_slider', 1);
+		update_option('flexslidergallery_MainImgSize', 348);
+		update_option('flexslidergallery_MediumImgSize', 148);
+		update_option('flexslidergallery_SmallImgSize', 58);
 	}
 }
 register_activation_hook(__FILE__, 'flexslidergallery_init_options');
 
 // 「プラグイン」メニューのサブメニュー
 function flexslidergallery_add_admin_menu() {
-	add_submenu_page('options-general.php', 'flexslidergalleryの設定', 'flexslidergalleryの設定', activate_plugins, __FILE__, 'flexslidergallery_admin_page');
+	add_submenu_page('options-general.php', 'FlexSlider Galleryの設定', 'FlexSlider Gallery', manage_options, __FILE__, 'flexslidergallery_admin_page');
 }
 add_action('admin_menu', 'flexslidergallery_add_admin_menu');
 
@@ -166,6 +169,9 @@ function flexslidergallery_admin_page() {
 	// 「変更を保存」ボタンがクリックされたときは、設定を保存する
 	if ($_POST['posted'] == 'Y') {
 		update_option('flexslidergallery_slider', intval($_POST['slider']));
+		update_option('flexslidergallery_MainImgSize', stripslashes($_POST['MainImgSize']));
+		update_option('flexslidergallery_MediumImgSize', stripslashes($_POST['MediumImgSize']));
+		update_option('flexslidergallery_SmallImgSize', stripslashes($_POST['SmallImgSize']));
 	}
 ?>
 <?php if($_POST['posted'] == 'Y') : ?><div class="updated"><p><strong>設定を保存しました</strong></p></div><?php endif; ?>
@@ -175,13 +181,13 @@ function flexslidergallery_admin_page() {
 		<input type="hidden" name="posted" value="Y">
 		<table class="form-table">
 			<tr valign="top">
-				<th scope="row"><label for="slider">表示タイプ<label></th>
+				<th scope="row"><label for="slider"><?php echo __( 'サムネイル設定', 'flexslidergallery' ); ?><label></th>
 				<td>
 					<select name="slider" id="slider">
 					<?php
 						$options = array(
-							array('value' => '1', 'text' => 'サムネイル付き'),
-							array('value' => '2', 'text' => 'サムネイルなし'),
+							array('value' => '1', 'text' => __( 'サムネイル付き', 'flexslidergallery' )),
+							array('value' => '2', 'text' => __( 'サムネイルなし', 'flexslidergallery' )),
 						);
 						foreach ($options as $option) : ?>
 						<option value="<?php echo esc_attr($option['value']); ?>"<?php if (get_option('flexslidergallery_slider') == $option['value']) : ?> selected="selected"<?php endif; ?>><?php echo esc_attr($option['text']); ?></option>
@@ -189,15 +195,38 @@ function flexslidergallery_admin_page() {
 					</select>
 				</td>
 			</tr>
+
+			<tr valign="top">
+				<th scope="row"><label for="MainImgSize"><?php echo __( 'ギャラリーのメイン画像サイズ', 'flexslidergallery' ); ?><label></th>
+				<td>
+					<input name="MainImgSize" type="text" id="MainImgSize" value="<?php echo esc_attr(get_option('flexslidergallery_MainImgSize')); ?>" class="regular-text code" /><br />
+					※画像サイズの縦横は同サイズになります。
+				</td>
+			</tr>
+
+			<tr valign="top">
+				<th scope="row"><label for="MediumImgSize"><?php echo __( 'ギャラリーのサムネイル画像の中サイズ', 'flexslidergallery' ); ?><label></th>
+				<td>
+				<input name="MediumImgSize" type="text" id="MediumImgSize" value="<?php echo esc_attr(get_option('flexslidergallery_MediumImgSize')); ?>" class="regular-text code" /><br />※画像サイズの縦横は同サイズになります。
+				</td>
+			</tr>
+
+			<tr valign="top">
+				<th scope="row"><label for="SmallImgSize"><?php echo __( 'ギャラリーのサムネイル画像の小サイズ', 'flexslidergallery' ); ?><label></th>
+				<td>
+					<input name="SmallImgSize" type="text" id="SmallImgSize" value="<?php echo esc_attr(get_option('flexslidergallery_SmallImgSize')); ?>" class="regular-text code" /><br />
+					※画像サイズの縦横は同サイズになります。
+				</td>
+			</tr>
 		</table>
 
 		<p class="submit">
-			<input type="submit" name="Submit" class="button-primary" value="変更を保存" />
+			<input type="submit" name="Submit" class="button-primary" value="<?php echo __( '変更を保存', 'flexslidergallery' ); ?>" />
 		</p>
 	</form>
-	<h2>プラグインの使い方</h2>
-	<p>テーマ内の表示したい箇所に下記のソースを表示してください。<br>
-	&lt;?php add_flexslider_gallery(); ?&gt;
+	<h2><?php echo __( 'プラグインの使い方', 'flexslidergallery' ); ?></h2>
+	<p><?php echo __( 'テーマ内の表示したい箇所に下記のソースを表示してください。', 'flexslidergallery' ); ?><br>
+	<?php echo __( '&lt;?php add_flexslider_gallery(); ?&gt;', 'flexslidergallery' ); ?>
 	</p>
 
 </div>
